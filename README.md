ae70c9010c1c37a4dcfe633e5dc7c70f584ca8ea
Marten Postma <postmam@corderius.nl>
2025-04-26 23:52:03 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coderius-play-pytest
-Pytest tests for coderius-play
+Pytest tests for coderius-play.
+- Please git clone the correct branch of coderius-play into this folder
+- cp the "play" module to the same folder as this file to make sure the import functions correctly.
+- call run_pytest_tests.bat
+- check pytest_stdout for the output of the tests
---
+++ run_pytest_tests.bat
@@ -0,0 +1,13 @@
+: object attributes
+pytest -q --tb=no --disable-warnings tests/objects_attributes/test_circle.py > pytest_stdout 
+pytest -q --tb=no --disable-warnings tests/objects_attributes/test_box.py >> pytest_stdout 
+pytest -q --tb=no --disable-warnings tests/objects_attributes/test_text.py >> pytest_stdout 
+
+: physics
+pytest -q --tb=no --disable-warnings tests/physics/test_x_movement.py >> pytest_stdout
+pytest -q --tb=no --disable-warnings tests/physics/test_y_movement.py >> pytest_stdout
+pytest -q --tb=no --disable-warnings tests/physics/test_physics_after_hide_show.py >> pytest_stdout
+
+: physics_collisions
+pytest -q --tb=no --disable-warnings tests/physics_collisions/test_steady_x_speed.py >> pytest_stdout
+pytest -q --tb=no --disable-warnings tests/physics_collisions/when_touching.py >> pytest_stdout
