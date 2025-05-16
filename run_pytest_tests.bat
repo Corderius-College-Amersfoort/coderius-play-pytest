6c827be1851170ee72b2b9a3660e34906296d9d3
Marten Postma <postmam@corderius.nl>
2025-05-16 10:28:56 +0200

    unpause works now

--- run_pytest_tests.bat
+++ run_pytest_tests.bat
@@ -6,6 +6,7 @@ pytest -q --tb=no --disable-warnings tests/objects_attributes/test_box.py >> pyt
 : physics
 pytest -q --tb=no --disable-warnings tests/physics/test_x_movement.py >> pytest_stdout
 pytest -q --tb=no --disable-warnings tests/physics/test_y_movement.py >> pytest_stdout
+pytest -q --tb=no --disable-warnings tests/physics/test_physics_after_pause_unpause.py >> pytest_stdout
 pytest -q --tb=no --disable-warnings tests/physics/test_physics_after_hide_show.py >> pytest_stdout
 
 : physics_collisions
